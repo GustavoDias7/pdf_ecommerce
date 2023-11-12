4127f4020b6fd95760089b391338fb0e24032014
Gustavo Dias <vboxuser@Ubuntu-Python.myguest.virtualbox.org>
2023-11-12 12:49:04 -0300

    Create settings

---
+++ pdf_ecommerce/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from core import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("product/<int:id>", views.product, name="prodcut"),
+]
+# Serving the media files in development mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += staticfiles_urlpatterns()
