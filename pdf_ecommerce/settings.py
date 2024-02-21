42b699a8a1977f707e5fd49fcf33b7495c7294d7
Gustavo Dias <gustavo7dias@gmail.com>
2024-02-21 11:20:34 -0300

    testing post_receive hook in Heroku

--- pdf_ecommerce/settings.py
+++ pdf_ecommerce/settings.py
@@ -221,3 +221,8 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
+
+COMPRESS_PRECOMPILERS = (
+    ('text/x-sass', 'django_libsass.SassCompiler'),
+    ('text/x-scss', 'django_libsass.SassCompiler'),
+)
